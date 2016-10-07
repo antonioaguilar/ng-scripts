d5ab00672f98179876f694a9fb786579ed549a94
Antonio Aguilar <aa6446694@gmail.com>
2016-10-07 01:43:58 +0100

    Release 1.0.5

--- ng.js
+++ ng.js
@@ -7,8 +7,8 @@ var args = process.argv;
 var flag = cli.getArguments(args);
 
 program.version(pkg.version);
-program.option('-a, --app', 'create new app');
-program.option('-s, --service', 'create service (factory)');
+program.option('-n, --new', 'create new app');
+program.option('-s, --service', 'create service');
 program.option('-c, --controller', 'create controller');
 program.option('-d, --directive', 'create directive');
 program.option('-C, --component', 'create component');
